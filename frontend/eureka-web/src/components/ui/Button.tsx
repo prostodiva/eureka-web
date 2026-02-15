9d60bfaffa35d7dd2f49020c21e8366ef784a6a3
prostodiva <rkattsyna@mail.ru>
2026-02-15 11:37:45 -0800

    Add: footer styling

--- frontend/eureka-web/src/components/ui/Button.tsx
+++ frontend/eureka-web/src/components/ui/Button.tsx
@@ -36,14 +36,14 @@ function Button({
   backgroundImage,
 }: ButtonProps) {
   const buttonClasses = classNames(
-    "flex justify-center items-center text-center font-inter",
+    'flex justify-center items-center text-center font-inter',
     {
       'flex justify-center items-center py-8 px-18 w-[15%] sm:w-[40%] h-auto aspect-[4/1] text-center rounded-sm bg-[#EA5D5D] text-white font-bold hover:bg-[#FFDC5C] whitespace-nowrap hover:text-[#8658F1]':
         variant === 'primary' && !backgroundImage,
-      'flex justify-center items-center py-8 px-18 w-[15%] sm:w-[40%]  h-auto aspect-[4/1] text-center rounded-sm bg-[#FFDC5C] text-[#8658F1] font-bold hover:bg-[#EA5D5D] whitespace-nowrap hover:text-white':
+      'flex justify-center items-center py-8 px-18 w-[15%] sm:w-[40%] h-auto aspect-[4/1] text-center rounded-sm bg-[#FFDC5C] text-[#8658F1] font-bold hover:bg-[#EA5D5D] whitespace-nowrap hover:text-white':
         variant === 'secondary' && !active && !backgroundImage,
-      'py-6 px-8 bg-[#3b3c5e] text-white':
-        variant === 'secondary' && active && !backgroundImage,
+      'flex justify-center items-center py-4 px-8 w-auto min-w-[80px] sm:min-w-[100px] h-auto aspect-[4/1] text-center rounded-sm bg-gray-50 text-black hover:bg-[#EA5D5D] whitespace-nowrap hover:text-white':
+        variant === 'ternary' && !active && !backgroundImage,
       'py-4 px-6 border bg-[#e93448] text-white hover:bg-white hover:text-black':
         variant === 'add' && !backgroundImage,
       'p-4 px-12 w-[8%] sm:w-[16%] h-auto bg-transparent':
--- frontend/eureka-web/src/root/Footer.tsx
+++ frontend/eureka-web/src/root/Footer.tsx
@@ -3,42 +3,83 @@ import Navbar from '../components/ui/Navbar.tsx';
 import { FaTelegramPlane } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa6';
 import { CgFacebook } from 'react-icons/cg';
+import Button from '../components/ui/Button.tsx';
 
 function Footer() {
   return (
-    <footer className="relative">
+    <footer className="relative flex flex-col items-center pb-20">
       {/*bg img*/}
       <div className="absolute">
         <img src={footer} alt="footer" className="w-full h-full object-cover" />
       </div>
 
       {/*content*/}
-      <div className="relative container mx-auto px-4 py-12">
-        <div className="grid grid-cols-3 md:grid-cols-3 gap-8 mb-8">
-          <div className="flex flex-row gap-4">
-            <FaTelegramPlane className="text-4xl text-white" />
-            <FaInstagram className="text-4xl text-white" />
-            <CgFacebook className="text-4xl text-white" />
+      <div className="relative mx-auto mt-44 pl-39">
+        <div className="grid grid-cols-3 md:grid-cols-3 mb-8">
+          {/*socials + form vertical block + EURIKA*/}
+          <div className="flex flex-col gap-8">
+            {/*socials*/}
+            <div className="flex flex-row gap-10">
+              <FaTelegramPlane className="text-6xl text-white" />
+              <FaInstagram className="text-6xl text-white" />
+              <CgFacebook className="text-6xl text-white" />
+            </div>
+            {/*email form button accept*/}
+            <div className="flex flex-col gap-4">
+              <form
+                className="border border-white rounded-sm w-190 h-24 flex items-center gap-4 p-4"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="flex-1 bg-transparent outline-none text-white"
+                  required
+                />
+                <Button type="submit" variant="ternary">
+                  Subscribe
+                </Button>
+              </form>
+
+              {/*checkbox + accept*/}
+              <div className="flex items-start gap-4 relative z-10">
+                <input
+                  type="checkbox"
+                  id="terms"
+                  required
+                  className="mt-1 w-6 h-6 sm:w-6 sm:h-6 cursor-pointer appearance-none border-2 border-gray-300 checked:bg-[#EA5D5D] checked:border-[#EA5D5D]"
+                />
+                <label
+                  htmlFor="terms"
+                  className="text-gray-300 text-xl underline underline-offset-3 cursor-pointer"
+                >
+                  I accept the Terms by subscribing you agree to with our
+                  Privacy Policy
+                </label>
+              </div>
+            </div>
+            <h1 className="font-inter text-white text-[15rem] font-normal -ml-3 -mt-25">
+              EUREKA
+            </h1>
           </div>
 
-          <div>
-            <h3 className="text-lg font-semibold mb-4">form</h3>
-            <p className="text-gray-300">I accept...</p>
-          </div>
-
-          <div>
-            <div className="flex gap-4">
+          {/*navbar*/}
+          <div className="flex flex-col gap-20">
+            <div className="flex gap-4 ml-82">
               <Navbar
                 variant="vertical"
                 navClassName="flex flex-col gap-2"
-                linkClassName="text-gray-300 hover:text-white transition flex items-center gap-2"
+                linkClassName="text-gray-300 hover:text-white transition flex items-center gap-2 text-3xl"
                 activeLinkClassName="font-bold text-white"
               />
             </div>
           </div>
         </div>
 
-        <div className="text-center text-gray-400">
+        {/* Copyright*/}
+        <div className="text-center text-gray-300 text-xl mt-50">
           <p>© 2026 Margarita Kattsyna</p>
         </div>
       </div>
