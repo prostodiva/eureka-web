77616f4ef658d8d80d51753d8ae83d76b7289e4b
prostodiva <rkattsyna@mail.ru>
2026-04-21 19:40:48 -0700

    fix

--- frontend/eureka-web/vite.config.ts
+++ frontend/eureka-web/vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import path from 'path';
+// @ts-expect-error critters types not resolving via package.json exports
+import Critters from 'critters'
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react(), cloudflare()],
+  plugins: [tailwindcss(), react(), cloudflare(), new Critters()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
